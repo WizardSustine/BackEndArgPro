c3e30dca3c7f41d0f4100d2a4d5ffee5370fe504
WizardSustine <bustamante.juan1990@gmail.com>
2022-11-14 17:53:56 -0300

    security update

---
+++ src/main/java/com/backendportfolio/BackEnd/Security/Service/RolService.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.backendportfolio.BackEnd.Security.Service;
+
+import com.backendportfolio.BackEnd.Security.Entity.Rol;
+import com.backendportfolio.BackEnd.Security.Enums.RolNombre;
+import com.backendportfolio.BackEnd.Security.Repository.iRolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Sustine
+ */
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return irolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
+}
